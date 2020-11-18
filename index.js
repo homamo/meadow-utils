d3c135b4eb169fdba3a14fed45ef5d8e57830584
Samuel Goudie <hello@sgoudie.com>
2020-11-18 12:23:51 +0000

    Added convert functions to index

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+import convertDecimalToPercent from './lib/convertDecimalToPercent';
+import convertPercentToDecimal from './lib/convertPercentToDecimal';
+import convertToPence from './lib/convertToPence';
+import convertToPounds from './lib/convertToPounds';
 import formatCurrency from './lib/formatCurrency';
 import formatDateTime from './lib/formatDateTime';
 import formatPercentage from './lib/formatPercentage';
 import round from './lib/round';
 
-export { formatCurrency, formatDateTime, formatPercentage, round };
+export {
+  convertDecimalToPercent,
+  convertPercentToDecimal,
+  convertToPence,
+  convertToPounds,
+  formatCurrency,
+  formatDateTime,
+  formatPercentage,
+  round,
+};
