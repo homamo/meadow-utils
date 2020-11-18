461809e7ecd080f3cae8da7d43235aa5b81d92a6
Samuel Goudie <hello@sgoudie.com>
2020-11-18 14:41:49 +0000

    Ported new utilities

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
+import capitalize from './lib/capitalize';
+import checkFileIsCsv from './lib/checkFileIsCsv';
+import checkUserHasOneOfRoles from './lib/checkUserHasOneOfRoles';
+import checkUserHasRole from './lib/checkUserHasRole';
+
 import convertDecimalToPercent from './lib/convertDecimalToPercent';
 import convertPercentToDecimal from './lib/convertPercentToDecimal';
 import convertToPence from './lib/convertToPence';
 import convertToPounds from './lib/convertToPounds';
+
+import formatAddress from './lib/formatAddress';
+import formatBytes from './lib/formatBytes';
 import formatCurrency from './lib/formatCurrency';
 import formatDateTime from './lib/formatDateTime';
 import formatPercentage from './lib/formatPercentage';
+import formatSnakeAsText from './lib/formatSnakeAsText';
+import formatUserName from './lib/formatUserName';
+
 import round from './lib/round';
+import saveFile from './lib/saveFile';
+import scrollToTop from './lib/scrollToTop';
 
 export {
+  capitalize,
+  checkUserHasOneOfRoles,
+  checkUserHasRole,
+  checkFileIsCsv,
   convertDecimalToPercent,
   convertPercentToDecimal,
   convertToPence,
   convertToPounds,
+  formatAddress,
+  formatBytes,
   formatCurrency,
   formatDateTime,
   formatPercentage,
+  formatSnakeAsText,
+  formatUserName,
   round,
+  saveFile,
+  scrollToTop,
 };
